35eb3c15572155608540398b0bd89598b3507dc2
Max Nordlund <max.nordlund@gmail.com>
2014-06-01 22:11:18 +0200

    Add language negotiation support

--- server.go
+++ server.go
@@ -2,26 +2,136 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"sort"
+	"strings"
 )
 
 const (
 	PORT   = 8080
 	STATIC = "static"
+	LANG   = "lang"
 )
 
+type Language struct {
+	Abbr, Country string
+	Quality       float64
+}
+
+type LanguageSlice []Language
+
+type StaticHandler struct {
+	Path string
+}
+
+type IndexHandler struct {
+	Files map[string]*StaticHandler
+}
+
+func newStaticHandler(path string) *StaticHandler {
+	return &StaticHandler{path}
+}
+
+func (sh StaticHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	http.ServeFile(rw, req, sh.Path)
+	log.Printf("Served request for %s", filepath.Base(sh.Path))
+}
+
+// Accept-Language:sv,en-US;q=0.8,en;q=0.6
+func parseLanguage(header string) (codes LanguageSlice) {
+	langs := strings.Split(header, ",")
+	codes = make([]Language, len(langs))
+	for i, lang := range langs {
+		code := Language{
+			Country: "",
+			Quality: 1,
+		}
+
+		pair := strings.Split(lang, ";")
+		if len(pair) == 2 {
+			fmt.Sscanf(pair[1], "q=%f", &code.Quality)
+		}
+
+		pair = strings.Split(pair[0], "-")
+		if len(pair) == 2 {
+			code.Country = pair[1]
+		}
+
+		code.Abbr = pair[0]
+		codes[i] = code
+	}
+	return
+}
+
+func (langs LanguageSlice) Len() int {
+	return len(langs)
+}
+
+func (langs LanguageSlice) Swap(i, j int) {
+	langs[i], langs[j] = langs[j], langs[i]
+}
+
+func (langs LanguageSlice) Less(i, j int) bool {
+	return langs[i].Quality > langs[j].Quality
+}
+
+func newIndexHandler(cwd string) (ih *IndexHandler) {
+	ih = &IndexHandler{
+		Files: make(map[string]*StaticHandler),
+	}
+	base := filepath.Join(cwd, LANG)
+	ls, err := ioutil.ReadDir(base)
+	die("ReadDir:", err)
+	for _, dir := range ls {
+		if dir.IsDir() {
+			path := filepath.Join(base, dir.Name(), "index.html")
+			ih.Files[dir.Name()] = newStaticHandler(path)
+		}
+	}
+	return
+}
+
+func (ih IndexHandler) MatchLanguage(header string) (*StaticHandler, Language) {
+	langs := parseLanguage(header)
+	sort.Stable(langs)
+	for _, lang := range langs {
+		if handler, ok := ih.Files[lang.Abbr]; ok {
+			return handler, lang
+		}
+	}
+	return ih.Files["en"], Language{}
+}
+
+func (ih IndexHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	handler, lang := ih.MatchLanguage(req.Header.Get("Accept-Language"))
+	handler.ServeHTTP(rw, req)
+	log.Printf("Best language match: %v", lang)
+}
+
+func static(cwd, filename string) {
+	sh := newStaticHandler(filepath.Join(cwd, STATIC, filename))
+	http.Handle(fmt.Sprintf("/%s", filename), sh)
+}
+
+func die(msg string, err error) {
+	if err != nil {
+		log.Fatal(msg, err)
+	}
+}
+
 func main() {
-	pwd, err := os.Getwd()
-	if err != nil {
-		log.Fatal("Getwd: ", err)
-	}
-	static := http.Dir(filepath.Join(pwd, STATIC))
-	log.Printf("Listen on %d and serving files in %s", PORT, static)
-	err = http.ListenAndServe(fmt.Sprintf(":%d", PORT), http.FileServer(static))
-	if err != nil {
-		log.Fatal("ListenAndServe:", err)
-	}
+	cwd, err := os.Getwd()
+	die("Getwd:", err)
+
+	http.Handle("/", newIndexHandler(cwd))
+	static(cwd, "style.css")
+	static(cwd, "favicon.ico")
+
+	path := filepath.Join(cwd, STATIC)
+	log.Printf("Listen on %d and serving files in %s", PORT, path)
+	die("ListenAndServe:", http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil))
 }
