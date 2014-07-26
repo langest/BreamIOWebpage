65708528141e34d122b05d158b6724b0d843c09e
Johan Fogelström <zapmew@gmail.com>
2014-07-26 14:30:01 +0200

    Main code

--- server.go
+++ server.go
@@ -1,138 +1,88 @@
-package main
+package breamiobeta
 
 import (
+	"appengine"
+	"appengine/datastore"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
-	"os"
-	"path/filepath"
-	"sort"
-	"strings"
 )
 
-const (
-	PORT   = 8080
-	STATIC = "static"
-	LANG   = "lang"
-)
+const form = `
+<html>
+	<head>
+		<title>Bream IO Beta Sign-up</title>
+		<link rel="stylesheet" type="text/css" href="/static/style.css">
+	</head>
+	<body>
+		<form action="/register" method="post">
+			<div class="field">
+				<label>Full name:</label>
+				<div>
+				<input type="text" name="fullname" size="40" placeholder="Bob Allison"></input>
+				</div>
+			</div>
+			<div class="field">
+				<label>E-mail:</label>
+				<div>
+				<input type="email" name="email" size="40" placeholder="awesome_bob94@gmail.com"></input>
+				</div>
+			</div>
+			<div class="field">
+				<label>Channel:</label>
+				<div>
+				<input type="text" name="channel" size="40" placeholder="http://twitch.tv/bob_io" ></input>
+				</div>
+			</div>
+			<div class="field"><input type="submit" value="Sign-up for beta" ></div>
+		</form>
+	</body>
+</html>
+`
 
-type Language struct {
-	Abbr, Country string
-	Quality       float64
+type Registration struct {
+	Name    string
+	Email   string
+	Channel string
 }
 
-type LanguageSlice []Language
-
-type StaticHandler struct {
-	Path string
+func init() {
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/register", register)
+	http.Handle("/thanks", ThankYouPage{})
 }
 
-type IndexHandler struct {
-	Files map[string]*StaticHandler
+// registrationkey returns the key used for all guestbook entries.
+func registrationKey(c appengine.Context) *datastore.Key {
+	// The string "default_guestbook" here could be varied to have multiple guestbooks.
+	return datastore.NewKey(c, "Registrations", "beta", 0, nil)
 }
 
-func newStaticHandler(path string) *StaticHandler {
-	return &StaticHandler{path}
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, form)
 }
 
-func (sh StaticHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	http.ServeFile(rw, req, sh.Path)
-	log.Printf("Served request for %s", filepath.Base(sh.Path))
-}
+func register(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
 
-// Accept-Language:sv,en-US;q=0.8,en;q=0.6
-func parseLanguage(header string) (codes LanguageSlice) {
-	langs := strings.Split(header, ",")
-	codes = make([]Language, len(langs))
-	for i, lang := range langs {
-		code := Language{
-			Country: "",
-			Quality: 1,
-		}
-
-		pair := strings.Split(lang, ";")
-		if len(pair) == 2 {
-			fmt.Sscanf(pair[1], "q=%f", &code.Quality)
-		}
-
-		pair = strings.Split(pair[0], "-")
-		if len(pair) == 2 {
-			code.Country = pair[1]
-		}
-
-		code.Abbr = pair[0]
-		codes[i] = code
+	reg := Registration{
+		Name:    r.FormValue("fullname"),
+		Email:   r.FormValue("email"),
+		Channel: r.FormValue("channel"),
 	}
-	return
-}
-
-func (langs LanguageSlice) Len() int {
-	return len(langs)
-}
-
-func (langs LanguageSlice) Swap(i, j int) {
-	langs[i], langs[j] = langs[j], langs[i]
-}
-
-func (langs LanguageSlice) Less(i, j int) bool {
-	return langs[i].Quality > langs[j].Quality
-}
-
-func newIndexHandler(cwd string) (ih *IndexHandler) {
-	ih = &IndexHandler{
-		Files: make(map[string]*StaticHandler),
-	}
-	base := filepath.Join(cwd, LANG)
-	ls, err := ioutil.ReadDir(base)
-	die("ReadDir:", err)
-	for _, dir := range ls {
-		if dir.IsDir() {
-			path := filepath.Join(base, dir.Name(), "index.html")
-			ih.Files[dir.Name()] = newStaticHandler(path)
-		}
-	}
-	return
-}
-
-func (ih IndexHandler) MatchLanguage(header string) (*StaticHandler, Language) {
-	langs := parseLanguage(header)
-	sort.Stable(langs)
-	for _, lang := range langs {
-		if handler, ok := ih.Files[lang.Abbr]; ok {
-			return handler, lang
-		}
-	}
-	return ih.Files["en"], Language{}
-}
-
-func (ih IndexHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	handler, lang := ih.MatchLanguage(req.Header.Get("Accept-Language"))
-	handler.ServeHTTP(rw, req)
-	log.Printf("Best language match: %v", lang)
-}
-
-func die(msg string, err error) {
+	fmt.Println(reg)
+	key := datastore.NewIncompleteKey(c, "Registration", registrationKey(c))
+	_, err := datastore.Put(c, key, &reg)
 	if err != nil {
-		log.Fatal(msg, err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
+	ThankYouPage{reg}.ServeHTTP(w, r)
 }
 
-func main() {
-	cwd, err := os.Getwd()
-	die("Getwd:", err)
-	ls, err := ioutil.ReadDir(filepath.Join(cwd, STATIC))
-	die("ReadDir:", err)
+type ThankYouPage struct {
+	Registration
+}
 
-	http.Handle("/", newIndexHandler(cwd))
-	for _, file := range ls {
-		if !file.IsDir() {
-			sh := newStaticHandler(filepath.Join(cwd, STATIC, file.Name()))
-			http.Handle(fmt.Sprintf("/%s", file.Name()), sh)
-			log.Printf("Registered handler for %s", file.Name())
-		}
-	}
-
-	log.Printf("Listen on %d", PORT)
-	die("ListenAndServe:", http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil))
+func (r Registration) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Thank you %s for your registration! We will get back to you when we are ready to start the beta.", r.Name)
 }
